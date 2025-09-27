bf8522ba64e84df64ed87d6b5cf7d302f22ea3e3
Florent Maillard <fmaillard@vaudoise.ch>
2025-09-27 10:03:48 +0200

    Add health

---
+++ handlers/health.go
@@ -0,0 +1,38 @@
+package handlers
+
+import (
+	"net/http"
+	"encoding/json"
+)
+
+type HealthHandler struct {
+	store storeHandler
+}
+
+type responseStatus struct {
+	Status	    string
+}
+
+func NewHealthHandler(store storeHandler) *HealthHandler {
+	return &HealthHandler{store: store}
+}
+
+// @Summary Get a status about the service
+// @Description Get the health status
+// @Tags Health
+// @Accept json
+// @Produce json
+// @Success 200 {object} models.Licence
+// @Failure 401 {object} models.ErrorResponse
+// @Failure 403 {object} models.ErrorResponse
+// @Failure 404 {object} models.ErrorResponse
+// @Router /health [get]
+func (h *HealthHandler) GetHealthStatus(w http.ResponseWriter, r *http.Request) {
+	response := map[string]string{
+        "Status": "healthy",
+    }
+    
+    w.Header().Set("Content-Type", "application/json")
+    json.NewEncoder(w).Encode(response)
+}
+	
---
+++ handlers/health_test.go
@@ -0,0 +1,54 @@
+package handlers
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"encoding/json"
+)
+
+func TestGetHealth(t *testing.T) {
+	tests := []struct {
+		name             string
+		mockStore        *mockStore
+		wantStatusCode   int
+		wantStatusMessage string
+	}{
+		{
+			name: "Retrieve status",
+			mockStore: nil,
+			wantStatusCode: http.StatusOK,
+			wantStatusMessage: "healthy",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			healthHandler := NewHealthHandler(tt.mockStore)
+
+			mux := http.NewServeMux()
+    		mux.HandleFunc("GET /health", healthHandler.GetHealthStatus)
+
+			w := httptest.NewRecorder()
+		    r := httptest.NewRequest("GET", "/health", nil)
+			
+			mux.ServeHTTP(w, r)
+
+			if w.Code != tt.wantStatusCode {
+				t.Errorf("expected status %d, got %d", tt.wantStatusCode, w.Code)
+			}
+
+			var got responseStatus
+
+			if err := json.Unmarshal(w.Body.Bytes(), &got); err != nil {
+				t.Fatalf("%v", w.Body)
+				t.Fatalf("failed to unmarshal response body: %v", err)
+			}
+
+			if got.Status != tt.wantStatusMessage {
+				t.Errorf("expected error message %q, got %q", tt.wantStatusMessage, got.Status)
+			}
+		})
+	}
+}
+
