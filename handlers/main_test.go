18ff870c3cd347b413c33fc34323acb3851499a5
flmailla <79896144+flmailla@users.noreply.github.com>
2025-09-29 16:02:29 +0200

    Fix coverage
    Co-authored-by: Florent Maillard <fmaillard@vaudoise.ch>

--- handlers/main_test.go
+++ handlers/main_test.go
@@ -2,9 +2,12 @@ package handlers
 
 import (
 	"testing"
+	"log/slog"
 	"github.com/flmailla/resume/logger"
+	"io"
 )
 
 func TestMain(m *testing.M) {
-	logger.InitLogger()
+	logger.Logger = slog.New(slog.NewJSONHandler(io.Discard, nil))
+	m.Run()
 }
